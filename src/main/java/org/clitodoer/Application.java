d5718c6e91d3089866163b21f62e4c06da89e7b2
Pramod Khalkar <pramodkhalkar7@gmail.com>
2025-07-02 00:19:23 +0530

    Initial commit with structure

---
+++ src/main/java/org/clitodoer/Application.java
@@ -0,0 +1,11 @@
+package org.clitodoer;
+
+/**
+ * @author : Pramod Khalkar
+ * @since : 02/07/25, Wed
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
